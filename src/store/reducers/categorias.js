a91b747f7e670cf67843da55ca86ef79214a0402
Aron Vinicius <aronvinicius15@gmail.com>
2024-02-13 20:34:00 -0300

    adding toasts

--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,5 +1,8 @@
+import { createStandaloneToast } from '@chakra-ui/toast';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import categoriesService from 'services/categoriesService';
+
+const { toast } = createStandaloneToast();
 
 const initialState = [];
 
@@ -13,7 +16,37 @@ const categoriasSlice = createSlice({
   initialState,
   extraReducers: (builder) => {
     builder
+      .addCase(fetchCategories.pending, (_, { payload }) => {
+        toast({
+          title: 'Carregando',
+          description: 'Carregando categorias',
+          status: 'loading',
+          duration: 1000,
+          isClosable: true
+        });
+
+        return payload;
+      })
       .addCase(fetchCategories.fulfilled, (_, { payload }) => {
+        toast({
+          title: 'Sucesso!',
+          description: 'Categorias carregadas com sucesso!',
+          status: 'success',
+          duration: 2000,
+          isClosable: true
+        });
+
+        return payload;
+      })
+      .addCase(fetchCategories.rejected, (_, { payload }) => {
+        toast({
+          title: 'Erro',
+          description: 'Erro na busca por categorias',
+          status: 'error',
+          duration: 2000,
+          isClosable: true
+        });
+
         return payload;
       })
   }
